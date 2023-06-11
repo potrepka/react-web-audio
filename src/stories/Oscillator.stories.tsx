d873ca21d6dfb6657a1f3ece2d92a1ec04cd2e10
Nathaniel Potrepka <nspotrepka@gmail.com>
2023-06-11 01:30:38 -0700

    Add controls to Storybook

--- src/stories/Oscillator.stories.tsx
+++ src/stories/Oscillator.stories.tsx
@@ -1,79 +1,147 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { useEffect, useMemo, useRef, useState } from 'react'
 import { Destination } from '../components/core/Destination'
 import { Gain } from '../components/core/Gain'
 import { Oscillator } from '../components/core/Oscillator'
 import { PlayButton } from './PlayButton'
 
+const typeOptions = ['sine', 'sawtooth', 'square', 'triangle']
+
 const meta: Meta<typeof Oscillator> = {
   component: Oscillator,
+  argTypes: {
+    connectTo: {
+      table: {
+        disable: true,
+      },
+    },
+    outputIndex: {
+      table: {
+        disable: true,
+      },
+    },
+    inputIndex: {
+      table: {
+        disable: true,
+      },
+    },
+    periodicWave: {
+      table: {
+        disable: true,
+      },
+    },
+    type: { control: 'select', options: typeOptions },
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof Oscillator>
 
-const renderOscillator = (oscillator: OscillatorNode | null) => (
-  <>
-    <p>Channel Count: {oscillator?.channelCount}</p>
-    <p>Channel Count Mode: {oscillator?.channelCountMode}</p>
-    <p>Channel Interpretation: {oscillator?.channelInterpretation}</p>
-    <p>Frequency: {oscillator?.frequency?.value}</p>
-    <p>Detune: {oscillator?.detune?.value}</p>
-    <p>Type: {oscillator?.type}</p>
-  </>
-)
-
 export const Basic: Story = {
-  render: () => {
-    const ref = useRef<OscillatorNode>(null)
-    const [oscillator, setOscillator] = useState<OscillatorNode | null>(null)
-
-    useEffect(() => {
-      setOscillator(ref.current)
-    }, [ref.current])
-
-    const info = useMemo(() => renderOscillator(oscillator), [oscillator])
-
+  args: {
+    type: 'sine',
+    frequency: 200,
+    detune: 0,
+  },
+  render: ({ type, frequency, detune }) => {
     return (
       <>
-        <Destination>
-          <Oscillator ref={ref} frequency={200} type="triangle" />
-        </Destination>
         <PlayButton />
-        {info}
+        <Destination>
+          <Oscillator type={type} frequency={frequency} detune={detune} />
+        </Destination>
       </>
     )
   },
 }
 
 export const AM: Story = {
-  render: () => (
+  args: {
+    type: 'sine',
+    frequency: 200,
+    // @ts-ignore
+    modulationType: 'sine' as OscillatorType,
+    modulationFrequency: 100,
+    modulationIndex: 1,
+  },
+  argTypes: {
+    detune: {
+      table: {
+        disable: true,
+      },
+    },
+    // @ts-ignore
+    modulationType: { control: 'select', options: typeOptions },
+  },
+  render: ({
+    type,
+    frequency,
+    // @ts-ignore
+    modulationType,
+    // @ts-ignore
+    modulationFrequency,
+    // @ts-ignore
+    modulationIndex,
+  }) => (
     <>
+      <PlayButton />
       <Destination>
-        <Gain gain={0.5}>
+        <Gain gain={1 / (1 + Math.abs(modulationIndex))}>
           <Gain>
-            <Oscillator frequency={200} />
-            <Oscillator frequency={100} connectTo={Gain.GAIN} />
+            <Oscillator type={type} frequency={frequency} />
+            <Gain gain={modulationIndex} connectTo={Gain.GAIN}>
+              <Oscillator
+                type={modulationType}
+                frequency={modulationFrequency}
+              />
+            </Gain>
           </Gain>
         </Gain>
       </Destination>
-      <PlayButton />
     </>
   ),
 }
 
 export const FM: Story = {
-  render: () => (
+  args: {
+    type: 'sine',
+    frequency: 200,
+    // @ts-ignore
+    modulationType: 'sine' as OscillatorType,
+    modulationFrequency: 100,
+    modulationIndex: 1,
+  },
+  argTypes: {
+    detune: {
+      table: {
+        disable: true,
+      },
+    },
+    // @ts-ignore
+    modulationType: { control: 'select', options: typeOptions },
+  },
+  render: ({
+    type,
+    frequency,
+    // @ts-ignore
+    modulationType,
+    // @ts-ignore
+    modulationFrequency,
+    // @ts-ignore
+    modulationIndex,
+  }) => (
     <>
+      <PlayButton />
       <Destination>
-        <Oscillator frequency={200}>
-          <Gain gain={200} connectTo={Oscillator.FREQUENCY}>
-            <Oscillator frequency={100} />
+        <Oscillator type={type} frequency={frequency}>
+          <Gain
+            gain={modulationIndex * modulationFrequency}
+            connectTo={Oscillator.FREQUENCY}
+          >
+            <Oscillator type={modulationType} frequency={modulationFrequency} />
           </Gain>
         </Oscillator>
       </Destination>
-      <PlayButton />
     </>
   ),
 }
