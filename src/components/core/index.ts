85a0ee98fab0ec737a8f36c94ac706e6f641baf3
Nathaniel Potrepka <nspotrepka@gmail.com>
2023-06-11 22:17:52 -0700

    Export Destination, Gain, and Oscillator

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -3,3 +3,6 @@ export {
   AudioContextProvider,
 } from './AudioContextProvider'
 export { AudioNodeContext, AudioNodeProvider } from './AudioNodeProvider'
+export { Destination } from './Destination'
+export { Gain } from './Gain'
+export { Oscillator } from './Oscillator'
